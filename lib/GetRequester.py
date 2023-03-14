2323541728b38afddfa6e6b2012fbc27338fd4c4
ajin3830 <ajin3830@gmail.com>
2023-03-14 17:08:12 -0400

    complete lab w notes

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,20 @@ import requests
 import json
 
 class GetRequester:
-
+# GetRequester class retrieves JSON from any provided URL
+    
+    # the url gets passed in on initialization  
     def __init__(self, url):
         self.url = url
 
+    # instance method to get the response body from the requested URL
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        # we use the JSON library to parse the API response into nicely formatted JSON
+        # meaning with the data we got back from the API we can collect and present it
+        # here we're just returning it
+        json_content = json.loads(self.get_response_body())
+        return json_content
